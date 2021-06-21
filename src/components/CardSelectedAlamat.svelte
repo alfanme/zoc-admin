f5544589b36ffcbb6c1e4e1afe23b3ac5d7731ef
Ahmad Alfan <aalf4nz@gmail.com>
2021-06-21 09:41:00 +0700

    Refactoring object revokes

--- src/components/CardSelectedAlamat.svelte
+++ src/components/CardSelectedAlamat.svelte
@@ -14,8 +14,11 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
     </button>
     <div class="text-wrapper">
-        <p class='penerima'>{data.penerima[0]}</p>
-        <p>Pengirim: {data.pengirim[0]}</p>
+        <p class='penerima'>{data.penerima.nama}</p>
+        <p>Pengirim: {data.pengirim.nama}</p>
+    </div>
+    <div>
+        <h3>{data.jumlah}</h3>
     </div>
 </div>
 
@@ -87,6 +90,11 @@
     }
 
     .text-wrapper {
+        margin: 0 16px;
         width: calc(100% - 105px);
+    }
+
+    h3 {
+        color: #4088FF;
     }
 </style>
