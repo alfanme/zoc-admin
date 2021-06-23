da8d9a14b3466dc79eca64a6df72460cb0ac84d3
Ahmad Alfan <aalf4nz@gmail.com>
2021-06-23 13:01:52 +0700

    Initiate tailwind migration

---
+++ src/components/Tailwind.svelte
@@ -0,0 +1,9 @@
+<style global>
+    @import 'tailwindcss/base';
+    @import 'tailwindcss/components';
+    @import 'tailwindcss/utilities';
+
+    body {
+        @apply bg-gray-900 text-gray-200;
+    }
+</style>
